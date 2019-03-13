b00b0c867a2583f46b7b5b528f9b1f954bc6f830
mfarah86 <47376986+mfarah86@users.noreply.github.com>
2019-03-12 20:12:08 -0700

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,26 +1,54 @@
-#ifndef PERSON_H
-#define PERSON_H
-
+#include <iostream>
 #include <string>
+#include "person.h"
 using namespace std;
-class Person
+
+Person::Person()
 {
-private:
-    string lastName;
-    string firstName;
-    float  payRate;
-    float  hoursWorked;
-public:
-    Person();
-    void setLastName(string lName);
-    string getLastName();
-    void setFirstName(string fName);
-    string getFirstName();
-    void setPayRate(float rate);
-    float getPayRate();
-    void setHoursWorked(float hours);
-    float getHoursWorked();
-    float totalPay();
-    string fullName();
-};
-#endif
+    string firstName = " ";
+    string lastName = " ";
+    float payRate = 0;
+    float hoursWorked = 0;
+}
+void Person::setLastName(string lName)
+{
+    lastName = lName;
+}
+string Person::getLastName()
+{
+    return lastName;
+}
+void Person::setFirstName(string fName)
+{
+    firstName = fName;
+}
+string Person::getFirstName()
+{
+    return firstName;
+}
+void Person::setPayRate(float rate)
+{
+    payRate = rate;
+}
+float Person::getPayRate()
+{
+    return payRate;
+}
+void Person::setHoursWorked(float hours)
+{
+    hoursWorked = hours;
+}
+float Person::getHoursWorked()
+{
+    return hoursWorked;
+}
+float Person::totalPay()
+{
+    float total = payRate * hoursWorked;
+    return total;
+}
+string Person::fullName()
+{
+    string lastName = firstName + " " + lastName;
+    return lastName;
+}
